31a2c4be4194f9934b24851c5bea388c9a935803
Panos Kouremenos <pank61985@gmail.com>
2023-12-05 14:39:10 +0200

    third message

--- git.java
+++ git.java
@@ -4,5 +4,6 @@ public class App{
     public static void main(String args[]){
         System.out.println(x: "I am first message");
         System.out.println(x: "I am the second message");
+        SYstem.out.println(x: "I am the third message");
     }
 }
