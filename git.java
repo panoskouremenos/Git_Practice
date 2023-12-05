539eb9c1ee0fd12e1c07ef75dc2a372aa275b94d
Panos Kouremenos <pank61985@gmail.com>
2023-12-05 13:17:40 +0200

    second message

---
+++ git.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App{
+    public static void main(String args[]){
+        System.out.println(x: "I am first message");
+        System.out.println(x: "I am the second message");
+    }
+}
